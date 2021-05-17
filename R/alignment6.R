e71e831587fa61bd5d1031a763804e578be0ec7b
Shir Dekel <shirdekel@yahoo.com.au>
2021-05-17 18:44:29 +1000

    Add data

---
+++ R/alignment6.R
@@ -0,0 +1,41 @@
+#' alignment6
+#' A data package for alignment6.
+#' @docType package
+#' @aliases alignment6-package
+#' @title Package Title
+#' @name alignment6
+#' @description A description of the data package
+#' @details Use \code{data(package='alignment6')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{data}
+NULL
+
+
+
+
+#' Detailed description of the data
+#' @name data
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{tbl_df} containing the following fields:
+#' \describe{
+#' \item{id}{}
+#' \item{variance}{}
+#' \item{hint}{}
+#' \item{sex}{}
+#' \item{age}{}
+#' \item{project}{}
+#' \item{ranking}{}
+#' \item{allocation}{}
+#' \item{npv_amount}{}
+#' \item{sample}{}
+#' \item{max_npv}{}
+#' \item{min_npv}{}
+#' \item{max_min_difference}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{alignment6}
+NULL
